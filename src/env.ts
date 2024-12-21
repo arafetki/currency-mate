0df40c819a17fc759f709dfc84fe3a0089c11b47
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-12-21 15:51:23 +0100

    fixed typo

--- src/env.ts
+++ src/env.ts
@@ -6,12 +6,12 @@ export const env = createEnv({
     CURRENCY_BEACON_API_KEY: z.string().min(1),
   },
   client: {
-    NEXT_PUBLIC_CURREANCY_BEACON_BASE_API_URL: z.string().url(),
+    NEXT_PUBLIC_CURRENCY_BEACON_BASE_API_URL: z.string().url(),
   },
   // For Next.js >= 13.4.4, you can just reference process.env:
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_CURREANCY_BEACON_BASE_API_URL:
-      process.env.NEXT_PUBLIC_CURREANCY_BEACON_BASE_API_URL,
+    NEXT_PUBLIC_CURRENCY_BEACON_BASE_API_URL:
+      process.env.NEXT_PUBLIC_CURRENCY_BEACON_BASE_API_URL,
   },
   emptyStringAsUndefined: true,
 });
